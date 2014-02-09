7352f3c7a00941f35b8ad9e01783125cd1f78308
sergei <sionin@outlook.com>
2014-02-09 15:02:19 +0400

    added optimal size calculation

--- bloom.py
+++ bloom.py
@@ -6,46 +6,54 @@ __author__ = 'sergei'
 
 
 class BloomFilter:
-
-    def __init__(self, size):
-        self.bitset = Bitset(0, size)
-        pass
-
-    def __contains__(self, item):
-        return self.bitset[self.hash1(item)] & self.bitset[self.hash2(item)] & self.bitset[self.hash3(item)] & self.bitset[self.hash4(item)] & self.bitset[self.hash5(item)]
-
-    def add(self, item):
-        self.bitset[self.hash1(item)] = True
-        self.bitset[self.hash2(item)] = True
-        self.bitset[self.hash3(item)] = True
-        self.bitset[self.hash4(item)] = True
-        self.bitset[self.hash5(item)] = True
-
-    def remove(self, item):
-        self.bitset[self.hash1(item)] = False
-        self.bitset[self.hash2(item)] = False
-        self.bitset[self.hash3(item)] = False
-        self.bitset[self.hash4(item)] = False
-        self.bitset[self.hash5(item)] = False
-
-    def hash1(self, string):
-        return self.hash(string, 13)
-
-    def hash2(self, string):
-        return self.hash(string, 17)
-
-    def hash3(self, string):
-        return self.hash(string, 31)
-
-    def hash4(self, string):
-        return self.hash(string, 41)
-
-    def hash5(self, string):
-        return self.hash(string, 59)
-
     def hash(self, string, prime):
         h = 0
         for c in string:
             h = (prime * h + ord(c)) & 0xFFFFFFFF
         bitset_length = (((h + 0x80000000) & 0xFFFFFFFF) - 0x80000000)
         return bitset_length % int(self.bitset.length)
+
+    def __init__(self, size, hashCount=0):
+        self.bitset = Bitset(0, size)
+        #2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157
+        hashFunctions = [
+            (lambda x: self.hash(x, 13))
+            , (lambda x: self.hash(x, 17))
+            , (lambda x: self.hash(x, 19))
+            , (lambda x: self.hash(x, 23))
+            , (lambda x: self.hash(x, 29))
+            , (lambda x: self.hash(x, 31))
+            , (lambda x: self.hash(x, 37))
+            , (lambda x: self.hash(x, 41))
+            , (lambda x: self.hash(x, 47))
+            , (lambda x: self.hash(x, 53))
+            , (lambda x: self.hash(x, 59))
+            , (lambda x: self.hash(x, 61))
+            , (lambda x: self.hash(x, 67))
+            , (lambda x: self.hash(x, 71))
+            , (lambda x: self.hash(x, 73))
+            , (lambda x: self.hash(x, 79))
+            , (lambda x: self.hash(x, 83))
+            , (lambda x: self.hash(x, 89))
+            , (lambda x: self.hash(x, 97))
+            , (lambda x: self.hash(x, 101))
+        ]
+        self.hashs = hashFunctions[0:hashCount or len(hashFunctions)]
+        pass
+
+    def __str__(self):
+        return "BloomFilter[size={} value={}]".format(self.bitset.length, self.bitset.__str__())
+
+    def __contains__(self, item):
+        contains = True
+        for h in self.hashs:
+            contains = contains & self.bitset[h(item)]
+        return contains
+
+    def add(self, item):
+        for h in self.hashs:
+            self.bitset[h(item)] = True
+
+    def remove(self, item):
+        for h in self.hashs:
+            self.bitset[h(item)] = False
--- test.py
+++ test.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
+from cmath import log
 import random
 import string
 
 from bloom import BloomFilter
 
+MAX_SIZE = 10000
+ERROR_PROBABILITY = 0.0001
 
-def generator(size=6, chars=string.ascii_uppercase):
+
+def generateValue(size=6, chars=string.ascii_uppercase):
     return ''.join(random.choice(chars) for x in range(size))
 
-bloom = BloomFilter(10000)
-print bloom.bitset
 
-for i in range(1000):
-    bloom.add(generator())
-
-print bloom.bitset
-
-print("MyString1" in bloom)
+def generateOther():
+    return generateValue(10, string.ascii_lowercase + string.digits)
 
 
+size = -(MAX_SIZE * log(ERROR_PROBABILITY)) / (log(2) * log(2))
+count = (size / MAX_SIZE) * log(2)
 
+optimalSize = int(complex(size).real)
+optimalCount = int(complex(count).real)
 
+print optimalSize, optimalCount
+
+bloom = BloomFilter(optimalSize, optimalCount)
+
+falsePositive = False
+inserted = 0;
+falseValue = "";
+
+while not falsePositive:
+
+    for i in range(MAX_SIZE / 100):
+        bloom.add(generateValue())
+        inserted += 1
+
+    for i in range(MAX_SIZE / 100):
+        falseValue = generateOther()
+        falsePositive = (falseValue in bloom)
+        if falsePositive:
+            break;
+
+print "========================="
+print "False positive at '", falseValue, "' after ", inserted, " insertions"
 
 
